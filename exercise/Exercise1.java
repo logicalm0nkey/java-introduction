3fedef1efe46cd546527856d0a852ce9d0d110a5
hiroaki-yamada <manigeo273511@gmail.com>
2021-12-19 14:06:15 +0900

    Update: リファクタリング

--- exercise/Exercise1.java
+++ exercise/Exercise1.java
@@ -8,14 +8,28 @@ public class Exercise1 {
     public static void main(String[] args) throws Exception {
         // InputStreamReader クラスを BufferedReader でラップ。
         // こうすることで、読み込んだ文字をバッファに蓄えられ、 readline() メソッドを使って1行ずつ読み込めるらしい。
+        // System.in がいわゆる stdinput らしい。
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         // 入力パラメータを読み込む
         String input = br.readLine();
         // 入力値を空白で分解する
-        String[] param = input.split(" ");
+        String[] params = input.split(" ");
 
-        calc(param);
+        if (!isIntegers(params)) {
+            System.out.println("整数を入力してください。");
+            return;
+        }
+
+        var val1 = Integer.valueOf(params[0]);
+        var val2 = Integer.valueOf(params[1]);
+
+        if (!isInRange(val1, val2)) {
+            System.out.println("-10000 以上 10000 以下で入力してください。");
+            return;
+        }
+
+        calc(val1, val2);
 
         br.close();
     }
@@ -25,20 +39,7 @@ public class Exercise1 {
      * 
      * @param param 入力値
      */
-    private static void calc(String[] param) {
-        if (!isIntegers(param)) {
-            System.out.println("整数を入力してください。");
-            return;
-        }
-
-        var val1 = Integer.valueOf(param[0]);
-        var val2 = Integer.valueOf(param[1]);
-
-        if (!isInRange(val1, val2)) {
-            System.out.println("-10000 以上 10000 以下で入力してください。");
-            return;
-        }
-
+    private static void calc(Integer val1, Integer val2) {
         System.out.println(val1.toString() + " + " + val2.toString() + " = "
                 + (val1.intValue() + val2.intValue()));
     }
@@ -46,16 +47,16 @@ public class Exercise1 {
     /**
      * 入力値が整数か判定する
      * 
-     * @param param 入力値
+     * @param params 入力値
      * @return true: 整数、 false: 整数でない
      */
-    private static boolean isIntegers(String[] param) {
+    private static boolean isIntegers(String[] params) {
         // 負の数を含めた整数か判定する正規表現
         Pattern pattern = Pattern.compile("-?\\d+");
 
         // 入力値が整数か判定
-        return pattern.matcher(param[0]).matches()
-                && pattern.matcher(param[1]).matches();
+        return pattern.matcher(params[0]).matches()
+                && pattern.matcher(params[1]).matches();
     }
 
     /**
