e38a2650f7aa43637a7482e9fc347e2b1088b37b
hiroaki-yamada <manigeo273511@gmail.com>
2021-12-22 09:43:11 +0900

    Update: Operator を enums package に移した

--- enums/Operator.java
+++ enums/Operator.java
@@ -1,4 +1,4 @@
-package exercise;
+package enums;
 
 /**
  * 演算子
--- exercise/Exercise2.java
+++ exercise/Exercise2.java
@@ -3,6 +3,7 @@ package exercise;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.regex.Pattern;
+import enums.Operator;
 
 public class Exercise2 {
     public static void main(String[] args) throws Exception {
@@ -35,7 +36,7 @@ public class Exercise2 {
     /**
      * 計算する
      * 
-     * @param leftVal 演算子の左辺
+     * @param leftVal  演算子の左辺
      * @param rightVal 演算子の右辺
      * @param operator 演算子
      */
