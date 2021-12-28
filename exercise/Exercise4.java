6c0788da15e16ba7497d24395861ed30f3310054
hiroaki-yamada <manigeo273511@gmail.com>
2021-12-28 14:21:34 +0900

    Update: fix

--- exercise/Exercise4.java
+++ exercise/Exercise4.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Arrays;
 import java.util.regex.Pattern;
 import enums.*;
 
