110f4d795754e987da5ac347a4f12039c46822e6
hiroaki-yamada <manigeo273511@gmail.com>
2021-12-29 10:58:41 +0900

    Update: 初めての forEach

--- exercise/GrammarExercise1.java
+++ exercise/GrammarExercise1.java
@@ -1,5 +1,7 @@
 package exercise;
 
+import java.util.Arrays;
+
 public class GrammarExercise1 {
     public static void main(String[] args) {
         System.out.println("引数の個数: " + args.length + "個");
@@ -7,5 +9,9 @@ public class GrammarExercise1 {
         for (int i = 0; i < args.length; i++) {
             System.out.println("引数 " + (i + 1) +" 番目は " + args[i]);
         }
+
+        System.out.println("forEach で出力");
+
+        Arrays.stream(args).forEach(param -> System.out.println(param));
     }
 }
