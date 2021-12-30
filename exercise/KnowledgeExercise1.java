46db7b9286db59e434d3ff401c0685d26696fdae
hiroaki-yamada <manigeo273511@gmail.com>
2021-12-30 14:50:30 +0900

    Fix: ファイルパスを起動時の引数で受け取るように修正。追記に関するコメントを記述。エラーハンドリング。 #1

--- exercise/KnowledgeExercise1.java
+++ exercise/KnowledgeExercise1.java
@@ -3,23 +3,33 @@ package exercise;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
 
 public class KnowledgeExercise1 {
     public static void main(String[] args) throws Exception {
-        File txtA = new File("resources/textA.txt");
-        File txtB = new File("resources/textB.txt");
-        try (BufferedReader br = Files.newBufferedReader(txtA.toPath(), Charset.forName("UTF8"));
-                PrintWriter pw = new PrintWriter(
-                        new BufferedWriter(new OutputStreamWriter(new FileOutputStream(txtB), "UTF8")))) {
-            String txt;
-            while ((txt = br.readLine()) != null) {
-                pw.println(txt);
+        try {
+            // resources/textA.txt
+            File txtA = new File(args[0]);
+            // resources/textB.txt
+            File txtB = new File(args[1]);
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(new FileInputStream(txtA), Charset.forName("UTF8")));
+                    // テキストをファイルに追記するときは FileOutputStream のコンストラクタの第2引数で true を指定する
+                    PrintWriter pw = new PrintWriter(
+                            new BufferedWriter(new OutputStreamWriter(new FileOutputStream(txtB), "UTF8")))) {
+                String txt;
+                while ((txt = br.readLine()) != null) {
+                    pw.println(txt);
+                }
             }
+        } catch (FileNotFoundException fe) {
+            System.out.println(fe.getMessage());
         }
     }
 }
