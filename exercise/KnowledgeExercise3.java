34e373e46e326ae8859468f48b5e6a6ae48cd339
hiroaki-yamada <manigeo273511@gmail.com>
2022-01-01 13:56:03 +0900

    Fix: try-with-resources のスコープを適切な範囲にした #1

--- exercise/KnowledgeExercise3.java
+++ exercise/KnowledgeExercise3.java
@@ -13,6 +13,9 @@ import java.util.ListIterator;
 
 public class KnowledgeExercise3 {
     public static void main(String[] args) throws Exception {
+        // テキスト格納用のリスト
+        List<String> lines = new ArrayList<String>();
+
         try {
             // resources/textA.txt
             File txtA = new File(args[0]);
@@ -20,28 +23,27 @@ public class KnowledgeExercise3 {
             try (BufferedReader br = new BufferedReader(
                     new InputStreamReader(new FileInputStream(txtA), Charset.forName("UTF8")))) {
                 String txt;
-                // テキスト格納用のリスト
-                List<String> lines = new ArrayList<String>();
 
                 while ((txt = br.readLine()) != null) {
                     // 格納
                     lines.add(txt);
                 }
-
-                // 格納した順番で出力
-                lines.forEach(e -> System.out.println(e));
-                // 逆順で出力
-                for (ListIterator<String> li = lines.listIterator(lines.size()); li.hasPrevious();) {
-                    System.out.println(li.previous());
-                }
-
-                // 「あいうえお」の行番号を取得
-                System.out.println(lines.indexOf("あいうえお") + 1);
             }
         } catch (FileNotFoundException fe) {
             System.out.println(fe.getMessage());
         } catch (IOException ie) {
             System.out.println(ie.getMessage());
         }
+
+        // 格納した順番で出力
+        lines.forEach(e -> System.out.println(e));
+        
+        // 逆順で出力
+        for (ListIterator<String> li = lines.listIterator(lines.size()); li.hasPrevious();) {
+            System.out.println(li.previous());
+        }
+
+        // 「あいうえお」の行番号を取得
+        System.out.println(lines.indexOf("あいうえお") + 1);
     }
 }
